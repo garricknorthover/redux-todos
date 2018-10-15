9a045a7aeac099a9b7d4c956686dfc928e0424a3
garricknorthover <garricknorthover@gmail.com>
2018-10-15 21:27:16 +1300

    something

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react'
 class TodoList extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-        todos: []
-    }
+    this.state = {}
+
+    const handleSubmit = (e) => {this.setState({todos: [...this.state.todos, this.state.formInput]})}  
+    const handleChange = (e) => {this.setState({formInput: e.target.value})}
   }
   render () {
     return <form onSubmit={this.handleSubmit}>
-        <input placeholder="add todo" onChange={this.onChange} />
+        <input placeholder="add todo" onChange={this.handleChange} />
         <input type="submit" value="Add" />
-    </form>
+    </form> 
   }
 }
 
