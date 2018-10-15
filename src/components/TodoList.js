a7eb6c696300c9baef43865da6bcb1f98ec5b795
garricknorthover <garricknorthover@gmail.com>
2018-10-15 21:52:23 +1300

    okky doky

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 
 class TodoList extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
-    this.state = {}
-
-    handleSubmit = (e) => {this.setState({todos: [...this.state.todos, this.state.formInput]})}  
-    handleChange = (e) => {this.setState({formInput: e.target.value})}
+    this.state = {
+      todos: [],
+      formInput: ''
+    }
   }
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({ todos: [...this.state.todos, this.state.formInput], formInput: '',
+     })
+  }
+  handleChange = e => {
+    this.setState({ formInput: e.target.value })
+  }
+
   render () {
-    return <form onSubmit={this.handleSubmit}>
-        <input placeholder="add todo" onChange={this.handleChange} />
-        <input type="submit" value="Add" />
-    </form> 
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder='add todo' onChange={this.handleChange} value={this.state.formInput} />
+        <input type='submit' value='Add' />
+      </form>
+      {this.state.todos.map(todo => <div>{todo}</div>)}
+      </div>
+    )
   }
 }
 
