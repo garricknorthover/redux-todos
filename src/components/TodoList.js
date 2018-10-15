5807f419cca25b57aa4900916f66365c87c6d53c
garricknorthover <garricknorthover@gmail.com>
2018-10-15 22:02:38 +1300

    still work in progress , about nested state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,14 +4,24 @@ class TodoList extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      todos: [],
+      todos: [
+        {
+          text: '',
+          id: 0
+        }
+      ],
       formInput: ''
     }
   }
   handleSubmit = e => {
     e.preventDefault()
-    this.setState({ todos: [...this.state.todos, this.state.formInput], formInput: '',
-     })
+    this.setState({
+      todos: {
+        text: [...this.state.todos.text, this.state.formInput],
+        formInput: ''
+      },
+      id: this.state.todos.id + 1
+    })
   }
   handleChange = e => {
     this.setState({ formInput: e.target.value })
@@ -20,11 +30,15 @@ class TodoList extends Component {
   render () {
     return (
       <div>
-      <form onSubmit={this.handleSubmit}>
-        <input placeholder='add todo' onChange={this.handleChange} value={this.state.formInput} />
-        <input type='submit' value='Add' />
-      </form>
-      {this.state.todos.map(todo => <div>{todo}</div>)}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder='add todo'
+            onChange={this.handleChange}
+            value={this.state.formInput}
+          />
+          <input type='submit' value='Add' />
+        </form>
+        {this.state.todos.map(todo => <div>{todo}</div>)}
       </div>
     )
   }
