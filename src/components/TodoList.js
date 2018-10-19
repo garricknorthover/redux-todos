1d3bf3407b6ca731b446c674b3d8d7786f610e70
garricknorthover <garricknorthover@gmail.com>
2018-10-19 22:47:20 +1300

    ghgh

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import * as actions from '../store/actions'
+import {addTodo, changeFormText} from '../store/actions'
 
 const TodoList = ({ addTodo, changeFormText, todos, formInput }) => (
   <div>
@@ -29,8 +29,8 @@ const TodoList = ({ addTodo, changeFormText, todos, formInput }) => (
 )
 
 const mapDispatchToProps = {
-  changeFormText: actions.changeFormText,
-  addTodo: actions.addTodo
+  changeFormText: changeFormText,
+  addTodo: addTodo
 }
 const mapStateToProps = state => ({
   todos: state.todos,
